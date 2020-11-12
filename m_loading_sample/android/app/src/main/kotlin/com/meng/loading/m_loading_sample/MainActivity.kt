16320c4025e66db1935cc275e264884044aafcf8
laomeng <781238222@qq.com>
2020-11-12 22:23:01 +0800

    add loading

---
+++ m_loading_sample/android/app/src/main/kotlin/com/meng/loading/m_loading_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.meng.loading.m_loading_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ m_loading_sample/m_loading/lib/m_loading.dart
@@ -0,0 +1,7 @@
+library m_loading;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
